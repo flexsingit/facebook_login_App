4430126b5271d3f2a1756f7f51d7c5ee0b90f7af
abhinav_chaudhary <abhinav_chaudhary@seologistics.com>
2018-09-26 17:52:27 +0530

    login App

---
+++ configuration/config.js
@@ -0,0 +1,12 @@
+module.exports={
+/*
+* This file contains the configurations information of Twitter login app.
+* It consists of Twitter app information, database information.
+*/
+
+	"facebook_api_key": 			"174577043432228",
+	"facebook_api_secret":				"d1e0e7a9f3fa0231509c1694ea57c6da",
+	"callback_url"				:				"http://localhost:3000/auth/facebook/callback",
+	"use_database"				:				"false",
+	"host"								:				"localhost",
+}
